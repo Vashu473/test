d7d09261ab4e172cf61f9cff8a0a0e7ebe39ae99
Vashu <vmanishkumar04@gmail.com>
2022-08-09 16:43:54 +0530

    cors removed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS"
   ),
-    res.setHeader("Access-Control-Allow-Origin", "http://192.168.1.14:5500");
+    res.setHeader("Access-Control-Allow-Origin", "http://192.168.1.14:5500/");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
