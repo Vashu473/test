c228146441716f9f00e4d1e5e671a228094acf74
Vashu <vmanishkumar04@gmail.com>
2022-08-09 16:26:02 +0530

    cors removed

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const User = require("./src/schema");
 const port = process.env.PORT || 80;
-app.all("*", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-  next();
-});
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 // starting db
 const startdb = async () => {
   await mongoose.connect(
