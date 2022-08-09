a1798f8efcb2d7019128660e6283c493b3120c95
Vashu <vmanishkumar04@gmail.com>
2022-08-09 16:42:37 +0530

    cors removed

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ const startdb = async () => {
     "mongodb+srv://vashu:vashudev143@cluster0.zaq0o.mongodb.net/?retryWrites=true&w=majority"
   );
 };
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS"
+  ),
+    res.setHeader("Access-Control-Allow-Origin", "http://192.168.1.14:5500");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(cors());
 // adding middleware
 app.use(express.json());
